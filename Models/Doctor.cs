1d147ea391c52885560ec2a537a8f6182b462b61
Marissa Balish <mb905315@ohio.edu>
2020-02-05 13:42:12 -0500

    edits

---
+++ Models/Doctor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mb905315_MIS4200.Models
+{
+    public class Doctor
+    {
+        public int doctorID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+
+        public ICollection<VisitDetails> VistDetails { get; set; }
+
+        // add any other fields as appropriate
+        // a customer can have any number of orders, a 1:M relationship,
+        // We represent this in the model with an ICollection
+        // The syntax says we are creating an ICollection of Order objects,
+        // (the name inside the <> is the object name),
+        // and the local name of the collection will be Order
+        // (the object name and the local name do not have to be the same)
+    }
+}
---
+++ Models/VisitDetails.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mb905315_MIS4200.Models
+{
+    public class VisitDetails
+    {
+    }
+}
